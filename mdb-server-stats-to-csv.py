1e02836ee7cfdb55f1e765fc8390f83326510226
pkdone <pkdone@gmail.com>
2019-07-06 14:37:29 +0100

    Removed some unneeded comments

--- mdb-server-stats-to-csv.py
+++ mdb-server-stats-to-csv.py
@@ -12,9 +12,6 @@
 #
 # Example (connecting to an Atlas cluster):
 #   $ ./mongo-stats-to-csv -u mongodb+srv://mainuser:mypswd@testcluster-abcde.mongodb.net
-#
-# See END OF THIS FILE for an example 'mongimport' command to take the content of the generated
-# CSV file and import it into a MongoDB database.
 ##
 import sys
 import os
@@ -128,10 +125,3 @@ DEFAULT_PERIODICITY_SECS = 10
 ##
 if __name__ == '__main__':
     main()
-
-
-"""
-# Mongoimport example command to import the generated CSV file into an Atlas MongoDB database:
-
-mongoimport --host StatsCluster-shard-0/statscluster-shard-00-00-s703u.mongodb.net:27017,statscluster-shard-00-01-s703u.mongodb.net:27017,statscluster-shard-00-02-s703u.mongodb.net:27017 --ssl --username main_user --password Password1 --authenticationDatabase admin --parseGrace=skipRow --ignoreBlanks --drop --db dbstats --collection statdata --type csv --columnsHaveTypes --fields "datetime.date(2006-01-02T15:04:05.000Z),mem_resident.double(),mem_virtual.double(),cache_bytes-dirty-in-the-cache-cumulative.double(),cache_tracked-dirty-bytes-in-the-cache.double(),cache_tracked-dirty-pages-in-the-cache.double(),cache_pages-written-requiring-in-memory-restoration.double(),cache_pages-selected-for-eviction-unable-to-be-evicted.double(),cache_pages-queued-for-urgent-eviction.double(),capacity_throttled-bytes-written-for-checkpoint.double(),capacity_throttled-bytes-written-for-eviction.double(),capacity_time-waiting-due-to-total-capacity-(usecs).double(),capacity_time-waiting-during-checkpoint-(usecs).double(),capacity_time-waiting-during-eviction-(usecs).double(),reconciliation_page-reconciliation-calls-for-eviction.double(),thread-yield_application-thread-time-evicting-(usecs).double(),thread-yield_page-acquire-eviction-blocked.double()" --file dbstats.csv
-"""
