4fd609a6b8847cfb0c210984e6164d36a06ceb46
Paul Done <pkdone@gmail.com>
2019-07-06 14:30:13 +0100

    Initial version of script and readme

---
+++ README.md
@@ -0,0 +1,23 @@
+# MongoDB Server Statistics To CSV Project
+
+Gathers MongoDB database server statistics (especially WiredTiger statistics) over time and appends these to a CSV file. The main Python script is useful where the MongoDB servers are remote and it is not possible to access the MongoDB servers' captured "Full Time Diagnostic Data Capture" ([FTDC](https://docs.mongodb.com/manual/administration/analyzing-mongodb-performance/#full-time-diagnostic-data-capture)) statistics, which will be the case for end users leveraging the [MongoDB Atlas](https://www.mongodb.com/cloud/atlas) DBaaS. Also see the MongoDB [t2](https://github.com/10gen/t2) time series visualisation tool.
+
+For usage first ensure the '.py' script is executable and then run:
+```
+./mongo-stats-to-csv.py -h
+```
+
+Example to connect to an Atlas cluster to collect statistics from:
+```
+./mongo-stats-to-csv -u mongodb+srv://mainuser:mypswd@testcluster-abcde.mongodb.net
+```
+
+To stop the collection of statistics to the CSV file just kill the Python script process, e.g. `Ctrl-C`.
+
+Optionally, you can load the generated CSV file into a new collection in a new MongoDB database and then use [MongoDB Charts](https://www.mongodb.com/products/charts) to better visualise its contents. Example command to import the generated CSV into a MongoDB Atlas cluster using [mongoimport](https://docs.mongodb.com/manual/reference/program/mongoimport/):
+```
+mongoimport --host StatsCluster-shard-0/statscluster-shard-00-00-s703u.mongodb.net:27017,statscluster-shard-00-01-s703u.mongodb.net:27017,statscluster-shard-00-02-s703u.mongodb.net:27017 --ssl --username main_user --password Password1 --authenticationDatabase admin --parseGrace=skipRow --ignoreBlanks --drop --db dbstats --collection statdata --type csv --columnsHaveTypes --fields "datetime.date(2006-01-02T15:04:05.000Z),mem_resident.double(),mem_virtual.double(),cache_bytes-dirty-in-the-cache-cumulative.double(),cache_tracked-dirty-bytes-in-the-cache.double(),cache_tracked-dirty-pages-in-the-cache.double(),cache_pages-written-requiring-in-memory-restoration.double(),cache_pages-selected-for-eviction-unable-to-be-evicted.double(),cache_pages-queued-for-urgent-eviction.double(),capacity_throttled-bytes-written-for-checkpoint.double(),capacity_throttled-bytes-written-for-eviction.double(),capacity_time-waiting-due-to-total-capacity-(usecs).double(),capacity_time-waiting-during-checkpoint-(usecs).double(),capacity_time-waiting-during-eviction-(usecs).double(),reconciliation_page-reconciliation-calls-for-eviction.double(),thread-yield_application-thread-time-evicting-(usecs).double(),thread-yield_page-acquire-eviction-blocked.double()" --file dbstats.csv
+```
+
+To see the list of default MongoDB/WiredTiger statistics gathered and generated into the CSV file, view the declared list of fields variables near the top of the Python script file. These list variables can be changed by the user, directly in the script. For all possible loggable fields see MongoDB's [serverStatus](https://docs.mongodb.com/manual/reference/command/serverStatus/) command. If you change what is logged to the CSV file, you will need to change the fields listed in the example _mongoimport_ command above, if you intend to import the generated CSV file into a MongoDB database.
+
---
+++ mdb-server-stats-to-csv.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+##
+# Gathers MongoDB database server statistics (especially WiredTiger statistics) over time and
+# appends these to a CSV file. This script is useful where the MongoDB servers are remote and it
+# is not possible to access the MongoDB servers' captured "Full Time Diagnostic Data Capture"
+# (FTDC) statistics, which will be the case for end users leveraging the MongoDB Atlas DBaaS.
+#
+# Also see the MongoDB 't2' time series visualisation tool: https://github.com/10gen/t2
+#
+# For usage first ensure the '.py' script is executable and then run:
+#   $ ./mongo-stats-to-csv.py -h
+#
+# Example (connecting to an Atlas cluster):
+#   $ ./mongo-stats-to-csv -u mongodb+srv://mainuser:mypswd@testcluster-abcde.mongodb.net
+#
+# See END OF THIS FILE for an example 'mongimport' command to take the content of the generated
+# CSV file and import it into a MongoDB database.
+##
+import sys
+import os
+import time
+import argparse
+import contextlib
+from datetime import datetime
+from pymongo import MongoClient
+
+
+# Core DB and WiredTiger server stats fields to log. For all possible loggable fields see MongoDB's
+# "serverStatus" command https://docs.mongodb.com/manual/reference/command/serverStatus/
+CORE_LOG_FIELDS = {
+    'mem': ['resident', 'virtual']
+}
+WT_LOG_FIELDS = {
+    'cache': ['bytes dirty in the cache cumulative', 'tracked dirty bytes in the cache',
+              'tracked dirty pages in the cache', 'pages written requiring in-memory restoration',
+              'pages selected for eviction unable to be evicted',
+              'pages queued for urgent eviction'],
+    'capacity': ['throttled bytes written for checkpoint', 'throttled bytes written for eviction',
+                 'time waiting due to total capacity (usecs)',
+                 'time waiting during checkpoint (usecs)',
+                 'time waiting during eviction (usecs)'],
+    'reconciliation': ['page reconciliation calls for eviction'],
+    'thread-yield': ['application thread time evicting (usecs)', 'page acquire eviction blocked']
+}
+
+
+##
+# Main function to gather stats and append to CSV file
+##
+def main():
+    argparser = argparse.ArgumentParser(description='Logs DB & WiredTiger statistics periodically '
+                                                    'to a local CSV file')
+    argparser.add_argument('-u', '--url', default=DEFAULT_MONGODB_URL,
+                           help=f'MongoDB Cluster URL (default: {DEFAULT_MONGODB_URL})')
+    argparser.add_argument('-c', '--csvfile', default=DEFAULT_CSV_FILENAME,
+                           help=f'Name of csv to log stats to (default: {DEFAULT_CSV_FILENAME})')
+    argparser.add_argument('-p', '--period', default=DEFAULT_PERIODICITY_SECS, type=int,
+                           help=f'The periodicity in seconds for polling for stats (default:'
+                                f'{DEFAULT_PERIODICITY_SECS})')
+    args = argparser.parse_args()
+    print(f'Generating stats for: "{args.url}" to {args.csvfile}')
+
+    with contextlib.suppress(FileNotFoundError):
+        os.remove(args.csvfile)
+
+    connection = MongoClient(host=args.url)
+    db = connection['nodb']
+
+    try:
+        with open(args.csvfile, 'w') as f:
+            f.write('datetime,')
+
+            for category in CORE_LOG_FIELDS.keys():
+                f.write(','.join([f'{category}_{key.replace(" ", "-")}'
+                                  for key in CORE_LOG_FIELDS[category]]))
+                f.write(',' if len(CORE_LOG_FIELDS[category]) > 0 else '')
+
+            for category in WT_LOG_FIELDS.keys():
+                f.write(','.join([f'{category}_{key.replace(" ", "-")}'
+                                  for key in WT_LOG_FIELDS[category]]))
+                f.write(',' if len(WT_LOG_FIELDS[category]) > 0 else '')
+
+            f.write("\n")
+            f.flush()
+
+            while True:
+                stats = db.command("serverStatus")
+                wt_stats = stats['wiredTiger']
+                f.write(f'{datetime.utcnow().isoformat()[:-3]}Z,')
+
+                for category in CORE_LOG_FIELDS.keys():
+                    f.write(','.join([str(stats[category][key])
+                                      for key in CORE_LOG_FIELDS[category]]))
+                    f.write(',' if len(CORE_LOG_FIELDS[category]) > 0 else '')
+
+                for category in WT_LOG_FIELDS.keys():
+                    f.write(','.join([str(wt_stats[category][key])
+                                      for key in WT_LOG_FIELDS[category]]))
+                    f.write(',' if len(WT_LOG_FIELDS[category]) > 0 else '')
+
+                f.write("\n")
+                f.flush()
+                time.sleep(args.period)
+    except KeyboardInterrupt:
+        shutdown()
+
+
+##
+# Swallow the verbiage that is spat out when using 'Ctrl-C' to kill the script
+##
+def shutdown():
+    print()
+
+    try:
+        sys.exit(0)
+    except SystemExit as e:
+        os._exit(0)
+
+
+# Constants
+DEFAULT_MONGODB_URL = 'mongodb://localhost:27017'
+DEFAULT_CSV_FILENAME = 'dbstats.csv'
+DEFAULT_PERIODICITY_SECS = 10
+
+
+##
+# Main
+##
+if __name__ == '__main__':
+    main()
+
+
+"""
+# Mongoimport example command to import the generated CSV file into an Atlas MongoDB database:
+
+mongoimport --host StatsCluster-shard-0/statscluster-shard-00-00-s703u.mongodb.net:27017,statscluster-shard-00-01-s703u.mongodb.net:27017,statscluster-shard-00-02-s703u.mongodb.net:27017 --ssl --username main_user --password Password1 --authenticationDatabase admin --parseGrace=skipRow --ignoreBlanks --drop --db dbstats --collection statdata --type csv --columnsHaveTypes --fields "datetime.date(2006-01-02T15:04:05.000Z),mem_resident.double(),mem_virtual.double(),cache_bytes-dirty-in-the-cache-cumulative.double(),cache_tracked-dirty-bytes-in-the-cache.double(),cache_tracked-dirty-pages-in-the-cache.double(),cache_pages-written-requiring-in-memory-restoration.double(),cache_pages-selected-for-eviction-unable-to-be-evicted.double(),cache_pages-queued-for-urgent-eviction.double(),capacity_throttled-bytes-written-for-checkpoint.double(),capacity_throttled-bytes-written-for-eviction.double(),capacity_time-waiting-due-to-total-capacity-(usecs).double(),capacity_time-waiting-during-checkpoint-(usecs).double(),capacity_time-waiting-during-eviction-(usecs).double(),reconciliation_page-reconciliation-calls-for-eviction.double(),thread-yield_application-thread-time-evicting-(usecs).double(),thread-yield_page-acquire-eviction-blocked.double()" --file dbstats.csv
+"""
